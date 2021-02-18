0ca1a6dfb8545e3dc5bf09b99d4a06222c28dc43
Federik94 <federico.deluca44@gmail.com>
2021-02-18 17:40:06 +0100

    Neopixel rosso modificato (Funzionante al 100%)

--- sketch/ricevitore 2.0 Con neopixel/RX_con_neopixel_rosso/RX_con_neopixel.ino_rosso/RX_con_neopixel.ino_rosso.ino
+++ sketch/ricevitore 2.0 Con neopixel/RX_con_neopixel_rosso/RX_con_neopixel.ino_rosso/RX_con_neopixel.ino_rosso.ino
@@ -8,8 +8,8 @@
 #include <avr/power.h> //controllo potenza neopixel
 #endif
 
-#define PIX 16 
-#define PIN 6
+#define PIX 12 //numero led
+#define PIN 6  // 
 
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel (PIX, PIN, NEO_GRB + NEO_KHZ800); // settaggio la neopixel
 int ReceivedMessage[1] = {000}; // Used to store value received by the NRF24L01
@@ -37,19 +37,42 @@ void loop(void)
   while (radio.available())
   {
     radio.read(ReceivedMessage, 1); // Read information from the NRF24L01
-
+    
     if (ReceivedMessage[0] == 111) // Indicates switch is pressed
     {
-      pixels.setPixelColor(16, pixels.Color(255,0,0)); // Colore rosso
+      pixels.setPixelColor(0, pixels.Color(255,0,0)); // Colore rosso 
+      pixels.setPixelColor(1, pixels.Color(255,0,0)); // Colore rosso1
+      pixels.setPixelColor(2, pixels.Color(255,0,0)); // Colore rosso
+      pixels.setPixelColor(3, pixels.Color(255,0,0)); // Colore rosso
+      pixels.setPixelColor(4, pixels.Color(255,0,0)); // Colore rosso
+      pixels.setPixelColor(5, pixels.Color(255,0,0)); // Colore rosso
+      pixels.setPixelColor(6, pixels.Color(255,0,0)); // Colore rosso
+      pixels.setPixelColor(7, pixels.Color(255,0,0)); // Colore rosso
+      pixels.setPixelColor(8, pixels.Color(255,0,0)); // Colore rosso
+      pixels.setPixelColor(9, pixels.Color(255,0,0)); // Colore rosso
+      pixels.setPixelColor(10, pixels.Color(255,0,0)); // Colore rosso
+      pixels.setPixelColor(11, pixels.Color(255,0,0)); // Colore rosso // Colore rosso
       pixels.show();
       Serial.println("sono accesso");
-      delay(3000);
+      delay(2000);
     }
     else
     {
-       pixels.setPixelColor(16, pixels.Color(0,0,0));
+       pixels.setPixelColor(0, pixels.Color(0,0,0));
+       pixels.setPixelColor(1, pixels.Color(0,0,0));
+       pixels.setPixelColor(2, pixels.Color(0,0,0));
+       pixels.setPixelColor(3, pixels.Color(0,0,0));
+       pixels.setPixelColor(4, pixels.Color(0,0,0));
+       pixels.setPixelColor(5, pixels.Color(0,0,0));
+       pixels.setPixelColor(6, pixels.Color(0,0,0));
+       pixels.setPixelColor(7, pixels.Color(0,0,0));
+       pixels.setPixelColor(8, pixels.Color(0,0,0));
+       pixels.setPixelColor(9, pixels.Color(0,0,0));
+       pixels.setPixelColor(10, pixels.Color(0,0,0));
+       pixels.setPixelColor(11, pixels.Color(0,0,0));
        pixels.show();
        Serial.println("sono spento");
+       delay(5);
     }
     delay(2);
    }
